44066c91839269b24993a0b982274eac9212e93c
landsea <wobushiludahai@163.com>
2020-01-28 21:01:26 +0800

    update 信号量及读写锁

---
+++ ch11/11_13.c
@@ -0,0 +1,43 @@
+/*
+ * @Author: land sea
+ * @Date: 2020-01-28 19:04:06
+ * @LastEditTime : 2020-01-28 19:15:57
+ * @LastEditors  : Please set LastEditors
+ * @Description: pthread_mutex_timedlock函数示例
+ * @FilePath: /apue/ch11/11_13.c
+ */
+#include "apue.h"
+#include <pthread.h>
+
+int main(void)
+{
+    int err;
+    struct timespec tout;
+    struct tm *tmp;
+    char buf[64];
+    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+    
+    pthread_mutex_lock(&lock);
+    printf("mutex is locked\n");
+    clock_gettime(CLOCK_REALTIME, &tout);
+    tmp = localtime(&tout.tv_sec);
+    strftime(buf, sizeof(buf), "%r", tmp);
+    printf("current time is %s\n", buf);
+    tout.tv_sec += 10;
+    
+    err = pthread_mutex_timedlock(&lock, &tout);
+    clock_gettime(CLOCK_REALTIME, &tout);
+    tmp = localtime(&tout.tv_sec);
+    strftime(buf, sizeof(buf), "%r", tmp);
+    printf("the time is now %s\n", buf);
+    if(err == 0)
+    {
+        printf("mutex locked again!\n");
+    }
+    else
+    {
+        printf("can't lock mutex again: %s\n", strerror(err));
+    }
+
+    exit(0);
+}
